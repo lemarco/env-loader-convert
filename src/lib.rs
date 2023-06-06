5b75f95d96208f3226268a28893cad57cf4f0240
Le Marc <lemarc@Les-MacBook-Pro.local>
2023-06-06 18:00:48 +0200

    testing validation params

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use regex::Regex;
 use std::str::FromStr;
-
 struct KeyValue {
     key: String,
-    ty: String,
+    value: String,
+    validation: String,
 }
 
 impl FromStr for KeyValue {
@@ -16,8 +17,53 @@ impl FromStr for KeyValue {
             return Err(());
         }
         let key = parts[0].trim().to_string();
-        let ty = parts[1].trim().to_string();
-        Ok(KeyValue { key, ty })
+        let value_and_validation: Vec<&str> = parts[1].trim().split("=>").collect();
+        if value_and_validation.len() != 2 {
+            return Ok(KeyValue {
+                key,
+                value: value_and_validation[0].trim().to_string(),
+                validation: "".to_string(),
+            });
+        }
+        let value = value_and_validation[0].trim().to_string();
+        let mut validation: [String; 4] = [
+            "".to_string(),
+            "".to_string(),
+            "".to_string(),
+            "".to_string(),
+        ];
+        value_and_validation[1].split(',').for_each(|func_literal| {
+            // let literal = func_literal.to_string().trim();
+            if func_literal.starts_with("min(") {
+                let re = Regex::new(r"\d+").unwrap();
+                if let Some(capture) = re.find(func_literal) {
+                    let num: i32 = capture.as_str().parse().unwrap();
+                    validation[0].push_str(&format!("{num}"));
+                } else {
+                    panic!()
+                }
+            } else if func_literal.starts_with("max(") {
+                let re = Regex::new(r"\d+").unwrap();
+                if let Some(capture) = re.find(func_literal) {
+                    let num: i32 = capture.as_str().parse().unwrap();
+                    validation[1].push_str(&format!("{num}"));
+                } else {
+                    panic!()
+                }
+            } else if func_literal == "notEmpty" {
+                validation[2].push('1');
+            } else if func_literal == "optional" {
+                validation[3].push('1');
+            } else {
+                panic!();
+            }
+        });
+
+        Ok(KeyValue {
+            key,
+            value,
+            validation: validation.join(","),
+        })
     }
 }
 
@@ -35,17 +81,24 @@ pub fn convert(input: TokenStream) -> TokenStream {
         .collect::<Result<Vec<KeyValue>, _>>()
         .unwrap_or_else(|_| panic!("Invalid input: {:?}", input));
 
-    let result = key_values.iter().map(|KeyValue { key, ty }| {
-        let key_str = key.as_str();
-        let value_enum = match ty.to_ascii_lowercase().as_str() {
-            "int" | "integer" | "i32" => quote! { Value::Int },
-            "str" | "string" => quote! { Value::Str },
-            "long" | "i64" => quote! { Value::Long },
-            "bool" | "boolean" => quote! { Value::Bool },
-            _ => panic!("Unsupported type: {:?}", ty),
-        };
-        quote! { (#key_str, #value_enum) }
-    });
+    let result = key_values.iter().map(
+        |KeyValue {
+             key,
+             value,
+             validation,
+         }| {
+            let key_str = key.as_str();
+            let value_enum = match value.to_ascii_lowercase().as_str() {
+                "int" | "integer" | "i32" => quote! { Value::Int },
+                "str" | "string" => quote! { Value::Str },
+                "long" | "i64" => quote! { Value::Long },
+                "bool" | "boolean" => quote! { Value::Bool },
+                "float" | "f32" => quote! { Value::Float },
+                _ => panic!("Unsupported type: {:?}", value),
+            };
+            quote! { (#key_str, #value_enum, #validation) }
+        },
+    );
 
     let output = quote! { [ #( #result ),* ] };
 
