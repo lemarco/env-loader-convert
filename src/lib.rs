a03880ae60319a6fd342a8dd7a2c41d6cbcf02b1
Le Marc <lemarc@Les-MacBook-Pro.local>
2023-06-06 18:20:16 +0200

    constraint impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,89 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use regex::Regex;
 use std::str::FromStr;
+
 struct KeyValue {
     key: String,
-    value: String,
-    validation: String,
+    ty: String,
+    constraints: Vec<Constraint>,
+}
+#[derive(Debug)]
+enum Constraint {
+    Min(i32),
+    Max(i32),
+    NotEmpty,
+    Optional,
 }
 
 impl FromStr for KeyValue {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(':').collect();
+        let mut parts: Vec<&str> = s.split(':').collect();
         if parts.len() != 2 {
             return Err(());
         }
         let key = parts[0].trim().to_string();
-        let value_and_validation: Vec<&str> = parts[1].trim().split("=>").collect();
-        if value_and_validation.len() != 2 {
-            return Ok(KeyValue {
-                key,
-                value: value_and_validation[0].trim().to_string(),
-                validation: "".to_string(),
-            });
-        }
-        let value = value_and_validation[0].trim().to_string();
-        let mut validation: [String; 4] = [
-            "".to_string(),
-            "".to_string(),
-            "".to_string(),
-            "".to_string(),
-        ];
-        value_and_validation[1].split(',').for_each(|func_literal| {
-            // let literal = func_literal.to_string().trim();
-            if func_literal.starts_with("min(") {
-                let re = Regex::new(r"\d+").unwrap();
-                if let Some(capture) = re.find(func_literal) {
-                    let num: i32 = capture.as_str().parse().unwrap();
-                    validation[0].push_str(&format!("{num}"));
-                } else {
-                    panic!()
-                }
-            } else if func_literal.starts_with("max(") {
-                let re = Regex::new(r"\d+").unwrap();
-                if let Some(capture) = re.find(func_literal) {
-                    let num: i32 = capture.as_str().parse().unwrap();
-                    validation[1].push_str(&format!("{num}"));
-                } else {
-                    panic!()
-                }
-            } else if func_literal == "notEmpty" {
-                validation[2].push('1');
-            } else if func_literal == "optional" {
-                validation[3].push('1');
-            } else {
-                panic!();
-            }
-        });
+        let constraints: Vec<Constraint> = parts[1]
+            .split(',')
+            .map(|s| s.trim().parse())
+            .collect::<Result<Vec<Constraint>, _>>()
+            .unwrap_or_else(|_| panic!("Invalid input: {:?}", parts[1]));
+
+        let ty =
+            constraints
+                .iter()
+                .rev()
+                .fold(
+                    parts.pop().unwrap().to_string(),
+                    |acc, constraint| match constraint {
+                        Constraint::Min(_) | Constraint::Max(_) => acc,
+                        _ => {
+                            let constraint_str = match constraint {
+                                Constraint::NotEmpty => "notEmpty",
+                                Constraint::Optional => "optional",
+                                _ => unreachable!(),
+                            };
+                            format!("{} => {}", constraint_str, acc)
+                        }
+                    },
+                );
 
         Ok(KeyValue {
             key,
-            value,
-            validation: validation.join(","),
+            ty,
+            constraints,
         })
     }
 }
 
+impl FromStr for Constraint {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        if s.starts_with("min(") && s.ends_with(')') {
+            let val = s[4..s.len() - 1]
+                .parse()
+                .unwrap_or_else(|_| panic!("Invalid constraint: {:?}", s));
+            Ok(Constraint::Min(val))
+        } else if s.starts_with("max(") && s.ends_with(')') {
+            let val = s[4..s.len() - 1]
+                .parse()
+                .unwrap_or_else(|_| panic!("Invalid constraint: {:?}", s));
+            Ok(Constraint::Max(val))
+        } else if s == "notEmpty" {
+            Ok(Constraint::NotEmpty)
+        } else if s == "optional" {
+            Ok(Constraint::Optional)
+        } else {
+            panic!("Invalid constraint: {:?}", s);
+        }
+    }
+}
+
 #[proc_macro]
-pub fn convert(input: TokenStream) -> TokenStream {
+pub fn convert_values(input: TokenStream) -> TokenStream {
     let input = input.to_string();
     let input = input.trim();
     let input = input.strip_prefix('{').unwrap_or(input);
@@ -84,19 +99,36 @@ pub fn convert(input: TokenStream) -> TokenStream {
     let result = key_values.iter().map(
         |KeyValue {
              key,
-             value,
-             validation,
+             ty,
+             constraints,
          }| {
             let key_str = key.as_str();
-            let value_enum = match value.to_ascii_lowercase().as_str() {
-                "int" | "integer" | "i32" => quote! { Value::Int },
-                "str" | "string" => quote! { Value::Str },
-                "long" | "i64" => quote! { Value::Long },
-                "bool" | "boolean" => quote! { Value::Bool },
-                "float" | "f32" => quote! { Value::Float },
-                _ => panic!("Unsupported type: {:?}", value),
+            let value_enum = match ty.as_str() {
+                "int" => {
+                    validate_int_constraints(constraints);
+                    quote! { Value::Int }
+                }
+                "str" => {
+                    validate_str_constraints(constraints);
+                    quote! { Value::Str }
+                }
+                "long" => {
+                    validate_long_constraints(constraints);
+                    quote! { Value::Long }
+                }
+                "bool" => {
+                    validate_bool_constraints(constraints);
+                    quote! { Value::Bool }
+                }
+                _ => panic!("Unsupported type: {:?}", ty),
             };
-            quote! { (#key_str, #value_enum, #validation) }
+            let constraints_tokens = constraints.iter().map(|constraint| match constraint {
+                Constraint::Min(val) => quote! { Constraint::Min(#val) },
+                Constraint::Max(val) => quote! { Constraint::Max(#val) },
+                Constraint::NotEmpty => quote! { Constraint::NotEmpty },
+                Constraint::Optional => quote! { Constraint::Optional },
+            });
+            quote! { (#key_str, #value_enum, &[ #( #constraints_tokens ),* ]) }
         },
     );
 
@@ -104,3 +136,56 @@ pub fn convert(input: TokenStream) -> TokenStream {
 
     output.into()
 }
+
+fn validate_int_constraints(constraints: &[Constraint]) {
+    for constraint in constraints {
+        match constraint {
+            Constraint::Min(_) | Constraint::Max(_) | Constraint::Optional => {}
+            _ => panic!("Invalid constraint for int: {:?}", constraint),
+        }
+    }
+}
+
+fn validate_str_constraints(constraints: &[Constraint]) {
+    for constraint in constraints {
+        match constraint {
+            Constraint::Min(_)
+            | Constraint::Max(_)
+            | Constraint::Optional
+            | Constraint::NotEmpty => {}
+            _ => panic!("Invalid constraint for str: {:?}", constraint),
+        }
+    }
+}
+
+fn validate_long_constraints(constraints: &[Constraint]) {
+    for constraint in constraints {
+        match constraint {
+            Constraint::Min(_) | Constraint::Max(_) | Constraint::Optional => {}
+            _ => panic!("Invalid constraint for long: {:?}", constraint),
+        }
+    }
+}
+
+fn validate_bool_constraints(constraints: &[Constraint]) {
+    for constraint in constraints {
+        match constraint {
+            Constraint::Optional => {}
+            _ => panic!("Invalid constraint for bool: {:?}", constraint),
+        }
+    }
+}
+
+enum Value {
+    Str,
+    Int,
+    Long,
+    Bool,
+}
+
+// enum Constraint {
+//     Min(i32),
+//     Max(i32),
+//     NotEmpty,
+//     Optional,
+// }
